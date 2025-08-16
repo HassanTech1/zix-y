c5eff8bea614e49bbd87fb202f0a0defa2e1e1be
まざはさん <tonypop1200@gmail.com>
2025-08-16 23:44:35 +0000

    fix the side menu

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -14,57 +14,59 @@ import { Home, Shield, Search, Book, Ticket, Bot, Siren, Settings, CircleAlert }
 export function AppSidebar() {
   return (
     <Sidebar variant="floating" collapsible="icon">
-      <SidebarContent>
-        <div className="flex justify-center p-2 mb-4">
-            <Icons.logo className="h-8 w-8 text-sidebar-foreground" />
-        </div>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Home">
-              <Home />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="/" isActive className="h-10 w-10 p-0" tooltip="Alerts">
-              <Shield />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Search">
-              <Search />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Runbook">
-              <Book />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Tickets">
-              <Ticket />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Automation">
-              <Bot />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Escalation">
-              <Siren />
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
+      <SidebarContent className="justify-between">
+        <div className="flex flex-col items-center gap-y-4">
+            <div className="flex justify-center p-2 mb-4">
+                <Icons.logo className="h-8 w-8 text-sidebar-foreground" />
+            </div>
+            <SidebarMenu>
             <SidebarMenuItem>
-                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Settings">
-                    <Settings />
+                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Home">
+                <Home />
                 </SidebarMenuButton>
             </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
+            <SidebarMenuItem>
+                <SidebarMenuButton href="/" isActive className="h-10 w-10 p-0" tooltip="Alerts">
+                <Shield />
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Search">
+                <Search />
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Runbook">
+                <Book />
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Tickets">
+                <Ticket />
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Automation">
+                <Bot />
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+                <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Escalation">
+                <Siren />
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+            </SidebarMenu>
+        </div>
+        <SidebarFooter>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton href="#" className="h-10 w-10 p-0" tooltip="Settings">
+                        <Settings />
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarFooter>
+      </SidebarContent>
     </Sidebar>
   );
 }
